8b7fcf418ca1bc13e7054e35a8e3da52d3639562
NicoGabrielGallegos <nicogabrielgallegos@gmail.com>
2025-10-02 15:53:59 -0300

    Definir entidades (fix)

--- src/calificacion/calificacion.entity.ts
+++ src/calificacion/calificacion.entity.ts
@@ -3,6 +3,7 @@ import { ObjectId } from "mongodb";
 export class Calificacion {
     constructor(
         public consulta: ObjectId,
-        public valoracion: number
+        public valoracion: number,
+        public _id?: ObjectId,
     ) {}
 }
