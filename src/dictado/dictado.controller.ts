f6c37da2cb3680672f8ca3f6338d750a1e017c8f
Nicolás Gallegos <nicogabrielgallegos@gmail.com>
2025-10-03 12:36:29 -0300

    CRUD Dictado (con validaciones)

--- src/dictado/dictado.controller.ts
+++ src/dictado/dictado.controller.ts
@@ -1,15 +1,43 @@
 import { Request, Response, NextFunction } from "express"
 import { DictadoRepository } from "./dictado.repository.js"
 import { Dictado } from "./dictado.entity.js"
+import { ObjectId } from "mongodb"
 
 const repository = new DictadoRepository()
 
-function sanitizeDictadoInput(req: Request, _res: Response, next: NextFunction) {
+function sanitizeDictadoInput(req: Request, res: Response, next: NextFunction) {
     req.body.sanitizedInput = {
         docente: req.body.docente,
         materia: req.body.materia
     }
 
+    const {docente, materia} = req.body.sanitizedInput
+
+    // Verificar docente
+    // Chequear undefined solo con el método POST
+    if (docente !== undefined || req.method === "POST") {
+        // Si es un id válido
+        if (ObjectId.isValid(docente))  {
+            req.body.sanitizedInput.docente = ObjectId.createFromHexString(docente)
+        } else {
+            res.status(400).send({message: "El id de docente ingresado no es válido"})
+            return
+        }
+    }
+
+    // Verificar materia
+    // Chequear undefined solo con el método POST
+    if (materia !== undefined || req.method === "POST") {
+        // Si es un id válido
+        if (ObjectId.isValid(materia))  {
+            req.body.sanitizedInput.materia = ObjectId.createFromHexString(materia)
+        } else {
+            res.status(400).send({message: "El id de materia ingresado no es válido"})
+            return
+        }
+    }
+
+    // Eliminar keys sin valores asignados
     Object.keys(req.body.sanitizedInput).forEach(key => {
         if(req.body.sanitizedInput[key] === undefined) {
             delete req.body.sanitizedInput[key]
