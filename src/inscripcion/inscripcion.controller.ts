9cb3e548a220dc69786379b9ff3b38376f1410a2
Nicolás Gallegos <nicogabrielgallegos@gmail.com>
2025-10-04 18:41:02 -0300

    CRUD Inscripcion
    Validaciones con integridad referencial y revamp del controlador

--- src/inscripcion/inscripcion.controller.ts
+++ src/inscripcion/inscripcion.controller.ts
@@ -2,44 +2,64 @@ import { Request, Response, NextFunction } from "express"
 import { InscripcionRepository } from "./inscripcion.repository.js"
 import { Inscripcion } from "./inscripcion.entity.js"
 import { ObjectId } from "mongodb"
+import { AlumnoRepository } from "../alumno/alumno.repository.js"
+import { ConsultaRepository } from "../consulta/consulta.repository.js"
+import { EstadoConsulta } from "../consulta/consulta.entity.js"
 
-const repository = new InscripcionRepository()
+const inscripcionRepository = new InscripcionRepository()
+const alumnoRepository = new AlumnoRepository()
+const consultaRepository = new ConsultaRepository()
 
-function sanitizeInscripcionInput(req: Request, res: Response, next: NextFunction) {
-    req.body.sanitizedInput = {
+function extractInput(req: Request, res: Response, next: NextFunction) {
+    req.body.input = {
         alumno: req.body.alumno,
         consulta: req.body.consulta,
     }
+    next()
+}
 
-    const {alumno, consulta} = req.body.sanitizedInput
+async function sanitizeInput(req: Request, res: Response, next: NextFunction) {
+    const {alumno, consulta} = req.body.input
+    req.body.sanitizedInput = {}
 
     // Verificar alumno
-    // Chequear undefined solo con el método POST
-    if (alumno !== undefined || req.method === "POST") {
-        // Si es un id válido
-        if (ObjectId.isValid(alumno)) {
-            req.body.sanitizedInput.alumno = ObjectId.createFromHexString(alumno)
-        } else {
+    // No chequear para undefined
+    if (alumno !== undefined) {
+        // Si el id no es válido
+        if (!ObjectId.isValid(alumno)) {
             res.status(400).send({ message: "El id de alumno ingresado no es válido" })
             return
         }
+        // Si no existe un alumno con la id ingresada
+        if (!(await alumnoRepository.findOne({ id: alumno }))) {
+            res.status(404).send({ message: "Alumno con id '" + alumno + "' no encontrado"})
+            return
+        }
+        // Sino, el alumno es válido
+        req.body.sanitizedInput.alumno = ObjectId.createFromHexString(alumno)
     }
 
     // Verificar consulta
-    // Chequear undefined solo con el método POST
-    if (consulta !== undefined || req.method === "POST") {
-        // Si es un id válido
-        if (ObjectId.isValid(consulta)) {
-            req.body.sanitizedInput.consulta = ObjectId.createFromHexString(consulta)
-        } else {
+    // No chequear para undefined
+    if (consulta !== undefined) {
+        // Si el id no es válido
+        if (!ObjectId.isValid(consulta)) {
             res.status(400).send({ message: "El id de consulta ingresado no es válido" })
             return
         }
+        // Si no existe una consulta con la id ingresada
+        const consultaRecuperada = await consultaRepository.findOne({ id: consulta })
+        if (!consultaRecuperada) {
+            res.status(404).send({ message: "Consulta con id '" + consulta + "' no encontrada"})
+            return
+        }
+        // Si se está creando la inscripción, pero la consulta ya finalizó o fue cancelada
+        if (req.method == "POST" && consultaRecuperada.estado !== EstadoConsulta.Programada) {
+            res.status(400).send({ message: "La consulta ingresada ya finalizó o fue cancelada" })
+        }
+        // Sino, la consulta es válida
+        req.body.sanitizedInput.consulta = ObjectId.createFromHexString(consulta)
     }
-
-    // TODO:
-    // - Integridad referencial
-    // - No poder inscribirse a una consulta con estado distinto a "Programada"
 
     Object.keys(req.body.sanitizedInput).forEach(key => {
         if (req.body.sanitizedInput[key] === undefined) {
@@ -50,11 +70,11 @@ function sanitizeInscripcionInput(req: Request, res: Response, next: NextFunctio
 }
 
 async function findAll(_req: Request, res: Response) {
-    res.json({ data: await repository.findAll() })
+    res.json({ data: await inscripcionRepository.findAll() })
 }
 
 async function findOne(req: Request, res: Response) {
-    const inscripcion = await repository.findOne({ id: req.params.id })
+    const inscripcion = await inscripcionRepository.findOne({ id: req.params.id })
     if (!inscripcion) {
         res.status(404).send({ message: "Inscripcion no encontrado" })
         return
@@ -65,12 +85,12 @@ async function findOne(req: Request, res: Response) {
 async function add(req: Request, res: Response) {
     const { alumno, consulta } = req.body.sanitizedInput
     const inscripcionInput = new Inscripcion(alumno, consulta)
-    const inscripcion = await repository.add(inscripcionInput)
+    const inscripcion = await inscripcionRepository.add(inscripcionInput)
     res.status(201).send({ message: "Inscripcion creado con éxito", data: inscripcion })
 }
 
 async function update(req: Request, res: Response) {
-    const inscripcion = await repository.update({ id: req.params.id }, req.body.sanitizedInput)
+    const inscripcion = await inscripcionRepository.update({ id: req.params.id }, req.body.sanitizedInput)
     if (!inscripcion) {
         res.status(404).send({ message: "Inscripcion no encontrado" })
         return
@@ -79,7 +99,7 @@ async function update(req: Request, res: Response) {
 }
 
 async function remove(req: Request, res: Response) {
-    const inscripcion = await repository.delete({ id: req.params.id })
+    const inscripcion = await inscripcionRepository.delete({ id: req.params.id })
     if (!inscripcion) {
         res.status(404).send({ message: "Inscripcion no encontrado" })
         return
@@ -87,4 +107,4 @@ async function remove(req: Request, res: Response) {
     res.status(200).send({ message: "Inscripcion borrado con éxito", data: inscripcion })
 }
 
-export { sanitizeInscripcionInput, findAll, findOne, add, update, remove }
+export { extractInput, sanitizeInput, findAll, findOne, add, update, remove }
--- src/inscripcion/inscripcion.routes.ts
+++ src/inscripcion/inscripcion.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { add, findAll, findOne, update, remove, sanitizeInscripcionInput } from "./inscripcion.controller.js";
+import { add, findAll, findOne, update, remove, extractInput, sanitizeInput } from "./inscripcion.controller.js";
+import { assureCompleteInput } from "../shared/controller.middlewares.js";
 
 export const inscripcionRouter = Router()
 
 inscripcionRouter.get("/", findAll)
 inscripcionRouter.get("/:id", findOne)
-inscripcionRouter.post("/", sanitizeInscripcionInput, add)
-inscripcionRouter.put("/:id", sanitizeInscripcionInput, update)
-inscripcionRouter.patch("/:id", sanitizeInscripcionInput, update)
+inscripcionRouter.post("/", extractInput, assureCompleteInput, sanitizeInput, add)
+inscripcionRouter.put("/:id", extractInput, sanitizeInput, update)
+inscripcionRouter.patch("/:id", extractInput, sanitizeInput, update)
 inscripcionRouter.delete("/:id", remove)
