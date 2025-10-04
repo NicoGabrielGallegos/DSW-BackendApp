02adef1b0b7157f887002c20f23059340cd9cb0a
Nicolás Gallegos <nicogabrielgallegos@gmail.com>
2025-10-04 17:13:17 -0300

    CRUD Docente
    Validaciones y revamp del controlador

--- src/docente/docente.controller.ts
+++ src/docente/docente.controller.ts
@@ -1,22 +1,63 @@
 import { Request, Response, NextFunction } from "express";
 import { DocenteRepository } from "./docente.repository.js";
 import { Docente } from "./docente.entity.js";
+import { isValidEmail } from "../shared/validations.js";
 
 const repository = new DocenteRepository()
 
-function sanitizeDocenteInput(req: Request, _res: Response, next: NextFunction) {
-    req.body.sanitizedInput = {
+function extractInput(req: Request, res: Response, next: NextFunction) {
+    req.body.input = {
         legajo: req.body.legajo,
         nombre: req.body.nombre,
         apellido: req.body.apellido,
         correo: req.body.correo
     }
+    next()
+}
 
+function assureCompleteInput(req: Request, res: Response, next: NextFunction) {
+    let error_message = "Entrada incompleta. Propiedades faltantes: "
+    let error = false
+
+    Object.keys(req.body.input).forEach(key => {
+        if (req.body.input[key] === undefined) {
+            error_message += key + ", "
+            error = true
+        }
+    })
+
+    if (error) {
+        res.status(400).send({ message: error_message.slice(0, -2)})
+        return
+    }
+
+    next()
+}
+
+function sanitizeInput(req: Request, res: Response, next: NextFunction) {
+    const { legajo, nombre, apellido, correo } = req.body.input
+    req.body.sanitizedInput = {
+        legajo: legajo,
+        nombre: nombre,
+        apellido: apellido,
+    }
+    
+    if (correo !== undefined) {
+        if (isValidEmail(correo)) {
+            req.body.sanitizedInput.correo = correo
+        } else {
+            res.status(400).send({ message: "El correo ingresado no es válido"})
+            return
+        }
+    }
+        
+    delete req.body.input
     Object.keys(req.body.sanitizedInput).forEach(key => {
         if(req.body.sanitizedInput[key] === undefined) {
             delete req.body.sanitizedInput[key]
         }
     })
+
     next()
 }
 
@@ -58,4 +99,4 @@ async function remove(req: Request, res: Response) {
     res.status(200).send({message: "Docente borrado con éxito", data: docente})
 }
 
-export {sanitizeDocenteInput, findAll, findOne, add, update, remove}
+export {extractInput, assureCompleteInput, sanitizeInput, findAll, findOne, add, update, remove}
--- src/docente/docente.routes.ts
+++ src/docente/docente.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { add, findAll, findOne, update, remove, sanitizeDocenteInput } from "./docente.controller.js";
+import { add, findAll, findOne, update, remove, extractInput, assureCompleteInput, sanitizeInput } from "./docente.controller.js";
 
 export const docenteRouter = Router()
 
 docenteRouter.get("/", findAll)
 docenteRouter.get("/:id", findOne)
-docenteRouter.post("/", sanitizeDocenteInput, add)
-docenteRouter.put("/:id", sanitizeDocenteInput, update)
-docenteRouter.patch("/:id", sanitizeDocenteInput, update)
+docenteRouter.post("/", extractInput, assureCompleteInput, sanitizeInput, add)
+docenteRouter.put("/:id", extractInput, sanitizeInput, update)
+docenteRouter.patch("/:id", extractInput, sanitizeInput, update)
 docenteRouter.delete("/:id", remove)
